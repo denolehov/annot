4680464ae3f956391622d56b866cc331ced1b522
Denis Olehov <denolehov@gmail.com>
2025-12-26 23:41:32 +0100

    Add composables/index.ts

---
+++ src/lib/composables/index.ts
@@ -0,0 +1,6 @@
+export { useSelection, type UseSelectionOptions, type LineInfo } from './useSelection.svelte';
+export { useAnnotations } from './useAnnotations.svelte';
+export { useExitModes } from './useExitModes.svelte';
+export { useKeyboard, type KeyboardHandlers, type KeyboardState } from './useKeyboard.svelte';
+export { useContentTracking } from './useContentTracking.svelte';
+export { useAnnotationEditor, type AnnotationEditorOptions } from './useAnnotationEditor.svelte';
