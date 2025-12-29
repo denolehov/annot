8b202737db13f8aa235b34e675b62197b8cff74a
Denis Olehov <denolehov@gmail.com>
2025-12-29 11:45:35 +0100

    feat: add command palette kbd hint to footer

--- src/lib/components/StatusBar.svelte
+++ src/lib/components/StatusBar.svelte
@@ -29,6 +29,7 @@
     </button>
   </div>
   <div class="status-bar-right">
+    <span class="kbd-hint"><kbd>:</kbd> command palette</span>
     <span class="kbd-hint"><kbd>g</kbd> global note</span>
     <span class="kbd-hint"><kbd>⌘w</kbd> save and close</span>
   </div>
